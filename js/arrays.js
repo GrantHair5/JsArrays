f6752a3593ff57644e28a2c84b9a8d78634418f9
Grant Hair <grant.hair@arnoldclark.com>
2021-05-05 10:35:03 +0100

    Add basic setup

---
+++ js/arrays.js
@@ -0,0 +1,70 @@
+let greekIslands = ["Santorini", "Crete", "Corfu", "Mykonos", "Naxos", "Kos"]
+
+// CRUD
+
+const addIsland = () =>{
+    //add islands here
+}
+
+const getIsland = (islandName) =>{
+    //get island here
+}
+
+const getIndexOfIsland = (islandName) =>{
+    //get island here
+}
+
+const displayIslandsOnFrontEnd = () =>{
+    const myAppDiv = document.getElementById("myApp");
+    //display islands here
+}
+
+const displayIslandsOnFrontEndAsString = () =>{
+    const myAppDiv = document.getElementById("myApp");
+    //display islands here
+}
+
+const updateIsland = (islandName) =>{
+    //update island here
+}
+
+const deleteIsland = () =>{
+    //delete islands here
+}
+
+//End Crud
+
+//Log Islands
+
+const logOutIslands = () =>{
+    //log islands here
+}
+
+const logOutIslandsUsingForeach = () =>{
+    //log islands here
+}
+
+const logOutIslandsUsingFor = () =>{
+    //log islands here
+}
+
+const logOutIslandsAsString = () =>{
+    //log islands here
+}
+
+//End Log Islands
+
+
+// Modify Data
+const uppercaseIsland = () =>{
+    //upperCase islands here
+}
+
+const sortIslandsAlphabetically = () =>{
+    //sort islands here
+}
+
+const splitListInTwo = () =>{
+    //split list here
+}
+// End Modify Data
